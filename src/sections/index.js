bc6fd8cc54324e6f602341d7ee132d4e7b900027
Kik106 <loriksh12@gmail.com>
2024-03-14 20:42:04 +0100

    NavBar

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CostumerReviews from "./CostumerReviews";
+import PopularProducts from "./PopularProducts";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services"
+import Footer from "./Footer";
+
+export  {
+    Hero,
+    CostumerReviews,
+    PopularProducts,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    Services,
+    Footer
+}
